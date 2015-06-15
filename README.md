5f265a7ef8d0321f77e765d994497b32e6b1398c
Beno Netto <beno@agenciagreen.com.br>
2015-06-15 20:29:51 -0300

    adding style and jquery
    adding jQuery lib CND and css style to the web app

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 ## Realtime Cloud Messaging PHP SDK
+Traduzido para português
+Original repository - [github Repo](https://github.com/realtime-framework/RealtimeMessaging-PHP)
+
 Part of the [The Realtime® Framework](http://framework.realtime.co), Realtime Cloud Messaging (aka ORTC) is a secure, fast and highly scalable cloud-hosted Pub/Sub real-time message broker for web and mobile apps.
 
+É parte de [The Realtime® Framework](http://framework.realtime.co), Realtime Cloud Messaging (aka ORTC) é um Pub / Sub agente de mensagens seguro, rápido e altamente escalável hospedado na nuvem em tempo real para web e aplicações móveis.
+
+
 If your application has data that needs to be updated in the user’s interface as it changes (e.g. real-time stock quotes or ever changing social news feed) Realtime Cloud Messaging is the reliable, easy, unbelievably fast, “works everywhere” solution.
+
+Se seu aplicativo tem dados que precisam ser atualizados na interface do usuário assim que atualizados (por exemplo, cotações de ações em tempo real ou em constante mudança feed de notícias sociais) Realtime Cloud Messaging é o confiável, fácil, incrivelmente rápido, e que funciona em qualquer lugar!
 
 
 ## API Reference
--- sample.php
+++ sample.php
@@ -11,7 +11,7 @@ $URL = 'http://ortc-developers.realtime.co/server/2.1';
 $AK = 'YOUR_APPLICATION_KEY';// your realtime.co application key
 $PK = 'YOUR_APPLICATION_PRIVATE_KEY';// your realtime.co private key
 $TK = 'YOUR_AUTHENTICATION_TOKEN';// token: could be randomly generated in the session
-$CH = 'MyChannel'; //channel
+$CH = 'myChannel'; //channel
 $ttl = 180; 
 $isAuthRequired = false;
 $result = false;
@@ -36,12 +36,83 @@ if($isAuthRequired){
 		), 
 		$ttl
 	);//post authentication permissions. w -> write; r -> read
-	echo 'authentication status '.( $result ? 'success' : 'failed' ).'<br/>';
+	echo '<div class="status-error">authentication status '.( $result ? 'success' : 'failed' ).'</div>';
 }
 
 if($result || !$isAuthRequired){
 	$result = $rt->send($CH, "Sending message from php API", $response);
-	echo ' send status '.( $result ? 'success' : 'failed' ).'<br/>';
+	
+	if($result){
+		
+		echo '<div class="status-ok"> send status connected</div>';
+	}else{
+		echo '<div class="status-error"> send status failed</div>';
+	
+	}
 }    
 
 ?>
+
+
+<!doctype html>
+<html>
+<head>
+    <title>Testando Realtime.co</title>
+	<style type="text/css">
+	body {
+	  color:#333;
+	  font-family:Arial,sans-serif;
+	}
+	
+	.status-ok{padding:5px; color:#fff; background:green; margin-top:10px; margin-bottom:10px; width:550px;}
+	.status-error{padding:5px; color:#fff; background:red; margin-top:10px; margin-bottom:10px; width:550px;}
+	#log{border-top:1px solid #ccc; padding:10px; margin-top:10px; width:550px; }
+	.msg{display:inline; border:1px solid #ccc; padding:3px; margin-top:3px; float:left; width:500px; height:auto;
+		color:#32CD32; background:#000; font-family: 'Courier New', Courier, 'Lucida Sans Typewriter', 'Lucida Typewriter', monospace;}
+	
+	</style>
+</head>
+<body>
+    <input type="text" id="message" />
+    <input type="button" onclick="sendMessage('myChannel');" value="Send to myChannel" />
+    <div id="log">
+    	
+    </div>
+
+    <script src="http://code.xrtml.org/xrtml-3.0.0.js"></script>
+	<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script>
+        var appkey = '<?php echo($AK); ?>',
+            url = '<?php echo($URL); ?>',
+            token = '<?php echo($TK); ?>';
+        xRTML.ready(function(){
+            xRTML.Config.debug = true;
+            xRTML.ConnectionManager.create(
+            {
+                id: 'myConn',
+                appkey: appkey,
+                authToken: token,
+                url: url,
+                channels: [
+                    {name: 'myChannel'}
+                ]
+            }).bind(
+            {
+                message: function(e) {
+                    var log = $("#log");
+                    log.prepend('<span class="msg">Message received: ' + e.message + '</span>');
+                }
+            });
+        });
+        function sendMessage(channel){
+            var msg = $('#message').val();
+            xRTML.ConnectionManager.sendMessage({
+                connections: ['myConn'],
+                channel: channel,
+                content: msg
+            });
+        }
+    </script>
+
+</body>
+</html>
